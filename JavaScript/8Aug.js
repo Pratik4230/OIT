198fcb0b1afc0fb6da3271ddf6c2bc9eb8afb5eb
Pratik4230 <pratikjadhav1438@gmail.com>
2024-08-08 09:28:34 +0530

    variables and scope

---
+++ JavaScript/8Aug.js
@@ -0,0 +1,80 @@
+
+
+var a =7;
+console.log(a);
+console.log(typeof(a));
+
+
+var b = "Hello";
+console.log(b);
+console.log(typeof b);
+
+var a = "bye";
+console.log(a);
+console.log(typeof a);
+
+
+//------------------------------------
+
+let x = 9;
+console.log(x);
+console.log(typeof(x));
+
+ x = "3.4";
+ console.log(x);
+ console.log(typeof(x));
+
+ //.......................
+
+ const g = 5;
+ console.log(g);
+ console.log(typeof(g));
+
+//   g = 45 cannot change const 
+ 
+ 
+
+ // scope
+
+var q = 10;
+console.log(q);
+
+{
+    var q = 23;
+    console.log(q);  
+}
+
+console.log(q);
+
+//letttttt
+
+let w = 10;
+console.log(w);
+
+{
+    let w = 23;
+    console.log(w);
+  w = "gjdk"
+    console.log(w);
+}
+
+console.log(w);
+
+//const
+
+  const t= 155;
+console.log(t);
+
+{
+    const t = 153;
+    console.log(t);
+}
+
+console.log(t);
+
+
+ 
+
+
+
+
