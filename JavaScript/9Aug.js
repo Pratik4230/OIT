0d495a500b80f706c70670fd8cc071c27486fe0e
Pratik4230 <pratikjadhav1438@gmail.com>
2024-08-09 08:59:35 +0530

    Operators

---
+++ JavaScript/9Aug.js
@@ -0,0 +1,65 @@
+console.log("Operators");
+
+//Arithematic +,-,*,/,%,**
+console.log("Arithematic +,-,*,/,%,**");
+let a = 15;
+let b = 5;
+
+console.log("a + b = " , a + b);
+console.log("a - b = " , a - b);
+console.log("a / b = " , a / b);
+console.log("a * b = " , a * b);
+console.log("a % b = " , a % b);
+console.log("a ** b = " ,a ** b);
+
+//Assignment Operators = , += , -= .... **=;
+console.log("Assignment Operators = , += , -= .... **=" );
+ a = 10;
+ b = 2;
+ 
+console.log("a = " , a);
+console.log("a += b = " , a += b);
+console.log("a -= b = " , a -= b);
+console.log("a /= b = " , a /= b);
+console.log("a *= b = " , a *= b);
+console.log("a %= b = " , a %= b);
+console.log("a **= b = " ,a **= b);
+
+//Increment and decrement
+console.log("Increment and decrement " );
+ a = 2;
+
+ console.log("a  = " , a );
+ console.log("++a " , ++a);
+console.log("a++ = " , a++ );
+console.log("a  = " , a );
+
+console.log("--a = " , --a );
+console.log("a-- = " , a--);
+console.log("a  = " , a );
+
+//comparison operator >, < ,>=, <= , == ,!= === , !== , ? :
+console.log("comparison operator >, < ,>=, <= , == ,!= === , !== , ? :");
+a = 5;
+b = 6;
+
+console.log("a > b " , a > b );
+console.log("a < b " , a < b );
+console.log("a >= b " , a >= b );
+console.log("a <= b " , a <= b );
+console.log("a == b " , a == b );
+console.log("a != b " , a != b );
+console.log("a === b " , a === b );
+console.log("a !== b " , a !== b );
+
+
+//Logical Operators && ,|| ,!
+console.log("Logical Operators  && ,|| ,!");
+
+a = 20;
+b = 20;
+let c = 25;
+
+console.log("a == b && b == c " , a == b && b == c);
+console.log("a == b || b == c " , a == b || b == c);
+console.log("a == b &&  a != c " , a == b &&  a != c);
