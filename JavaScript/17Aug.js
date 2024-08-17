da2da422066649e82014e948cb922ee3c2539916
Pratik4230 <pratikjadhav1438@gmail.com>
2024-08-17 09:09:49 +0530

    arrow functions

---
+++ JavaScript/17Aug.js
@@ -0,0 +1,14 @@
+const r = 7;
+const h = 5;
+
+function volumeOfCylinder(r, h) {
+  return 3.14 * (r * r) * h;
+}
+
+let volOfCyl = (r, h) => 3.14 * (r * r) * h;
+ 
+
+let ans = volumeOfCylinder(r, h);
+   console.log(ans.toFixed(2));
+
+
