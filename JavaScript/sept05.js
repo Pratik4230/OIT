2834f9eaa2c2b0eace967c8acf0976e55dc00b65
Pratik4230 <pratikjadhav1438@gmail.com>
2024-09-05 16:23:24 +0530

    the end ?

---
+++ JavaScript/sept05.js
@@ -0,0 +1,27 @@
+
+let b = document.getElementById("btn");
+
+b.addEventListener('click' , () => {
+    // window.alert("Button Clickesd")
+
+    // window.open("https://www.youtube.com" , "_blank","width=200,hseight=150 , top=250 , left=350");
+    // window.open("https://www.youtube.com" , "_self");
+
+    // console.log(window.innerHeight);
+    // console.log(window.outerHeight);
+    // console.log(window.innerWidth);
+    // console.log(window.outerWidth);
+
+    // location.assign("https://www.example.com")
+    location.replace("https://www.example.com")
+    location.replace()
+
+// console.log(navigator.userAgent);
+// console.log(navigator.cookieEnabled);
+
+console.log(screen);
+
+   
+
+
+})
